5464749fb1c846c9ab86262f10093860334bd6b4
Brit Wallace <britwallace888@gmail.com>
2022-01-13 20:02:35 -0800

    Exchange rate for single index works
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>
    Co-authored-by: Amy Young <youngamy1223@gmail.com>

--- src/crypto-service.js
+++ src/crypto-service.js
@@ -1,6 +1,14 @@
 export default class CryptoService{
-static getCrypto(exchange){
-  return fetch(``)
+static getCrypto(){
+  return fetch(`https://api.nomics.com/v1/exchange-rates?key=${process.env.API_KEY}`)
+  .then(function(response) {
+    if (!response.ok){
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .catch(function(error) {
+    return error;
+  })
 }
-
 }
--- src/index.js
+++ src/index.js
@@ -3,3 +3,114 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CryptoService from './crypto-service.js';
+
+function clearFields() {
+  $('#text').val("");
+}
+
+function getElements(response, dollars) {
+  if (response) {
+    $('.showRate').text(`The exchange rate is ${response[25].rate * dollars}`);
+    $('.showCurrency').text(`The currency ${response[25].currency}`);
+    console.log(response);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const currencyInput = parseFloat($("input#text").val());
+    clearFields();
+    CryptoService.getCrypto()
+      .then(function(response) {
+        getElements(response, currencyInput);
+    
+      });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+    // const response = async function()  {
+    //   let cryptoService = new CryptoService();
+    //   const response = await cryptoService.getCrypto();
+    //   return response;
+    // };
+    // console.log(response);
+//   });
+// });
+
+
+
+//       getElements(response);
+//     })();
+
+//     const getElements = function(response) {
+//       if (response === false) {
+//         $("#showAmount").append("There has been an error. Please try again!")
+//         $("#showAmount").show();
+//       } else if (response.cryptoService.length === 0) {
+//         $("#showAmount").append("There are no results. Please try again.")
+//         $("#showAmount").show();
+//       } else if (response.cryptoService.length > 0 ) {
+//         $("#showAmount").show();
+//         let cryptoValue = ""
+//         response.cryptoService.forEach(function(element) {
+//           Object.keys(this.currency)
+
+//           cryptoValue.concat(cryptoService.//converted value here//)
+//         });
+//       });
+//     });
+//   });
+// };
+
+
+// from api using fetch weather app
+
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+// import WeatherService from './weather-service.js'
+// import tempConverter from './tempConverter.js'
+
+// function clearFields() {
+//   $('#city').val("");
+//   $('#state').val("");
+//   $('.showErrors').text("");
+//   $('.showHumidity').text("");
+//   $('.showTemp').text("");
+//   $('.showWindSpeed').text("");
+// }
+
+// function getElements(response) {
+//   if (response.main) {
+//     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+//     $('.showTemp').text(`The temperature ${response.main.temp} degrees Kelvin, or ${tempConverter(response.main.temp)} degrees Fahrenheit.`);
+//     $('.showWindSpeed').text(`The wind speed is ${response.wind.speed} m/s.`);
+//   } else {
+//     $('.showErrors').text(`There was an error: ${response.message}`);
+//   }
+// }
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     const city = $('#city').val();
+//     const state = $('#state').val();
+//     clearFields();
+//     WeatherService.getWeather(city, state)
+//       .then(function(response) {
+//         getElements(response);
+//       });
+//   });
+// });
